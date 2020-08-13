cd9f99d4a2d2598d8083378893bd1cf239fe1a6b
Josh Pechner <joshpechner@gmail.com>
2020-08-13 14:55:52 -0700

    added react

---
+++ my-app/src/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="Banner">
+        <div className="Banner-content">
+            My App, Bruh
+        </div>
+    </div>
+  );
+}
+
+export default Banner;
